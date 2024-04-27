c0db49df7678035fa024ec9011d0a30e9fffd0e2
William Knight <William.Jeffery.Knight@protonmail.com>
2024-04-27 11:16:13 -0400

    display the stream

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-alert("Hello World!!!")
+class UserCamera {
+    constructor() {
+        this.video = document.getElementById('video')
+        this.snapErrorMsgElement = document.getElementById('snapErrorMsgElement')
+        // video properties
+        this.constraints = {
+            audio: false,
+            video: {
+                width: 1280,
+                height: 720
+            }
+        }
+
+    }
+    // display the stream
+    handleSuccess(stream) {
+        window.stream = stream;
+        this.video.srcObject = stream;
+        console.log(stream)
+    }
+    // ask the user permission to use webcam
+    async init() {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia(this.constraints);
+            this.handleSuccess(stream)
+        } catch (e) {
+            this.snapErrorMsgElement.innerHTML = `navigator.mediaDevices.getUserMedia.error: ${e.toString()}`
+        }
+    }
+
+    // download Image
+
+    // count down before picture is taken
+
+
+}
+
+const cam1 = new UserCamera();
+cam1.init()
