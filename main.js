a1f27ca19659f3d070e4f9d21705657ae3b6549b
William Knight <William.Jeffery.Knight@protonmail.com>
2024-04-27 11:27:56 -0400

    can capture a picture from webcam

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 class UserCamera {
     constructor() {
         this.video = document.getElementById('video')
+        this.snap = document.getElementById('snap')
+        this.canvas = document.getElementById('canvas')
         this.snapErrorMsgElement = document.getElementById('snapErrorMsgElement')
+        this.countDisplay = document.getElementById('countDisplay')
         // video properties
         this.constraints = {
             audio: false,
@@ -10,7 +13,12 @@ class UserCamera {
                 height: 720
             }
         }
-
+        // make capture active
+        this.listener = this.snap.addEventListener("click", () => {
+            snap.disabled = true;
+            console.log(this.canvas)
+            this.countDown(this.saveImg1, this.video, this.canvas)
+        })
     }
     // display the stream
     handleSuccess(stream) {
@@ -28,10 +36,37 @@ class UserCamera {
         }
     }
 
-    // download Image
+    // download Image   
+    saveImg1(video, canvas) {
+        const context = canvas.getContext('2d');
+        context.drawImage(video, 0, 0, 640, 360);
+        const dataURI = canvas.toDataURL();
+        const a = document.createElement("a")
+        document.body.appendChild(a)
+        a.href = dataURI;
+        a.download = "selfie.png"
+        a.click()
+        document.body.removeChild(a)
+    }
 
     // count down before picture is taken
-
+    countDown(func, video, canvas) {
+        let count = 5;
+        const timer = setInterval(function () {
+            count--;
+            console.log(count);
+            this.countDisplay.innerHTML = count
+            if (count === 1) this.countDisplay.innerHTML = Cheese
+            if (count === 0) {
+                clearInterval(timer);
+                console.log("Time's up!");
+                console.log(this.canvas)
+                func(video, canvas)
+                this.countDisplay.innerHTML = 5
+                snap.disabled = false;
+            }
+        }, 1000);
+    }
 
 }
 
